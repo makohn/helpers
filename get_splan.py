92f68d3b9382940789e6ca0db542cff8ebbcc805
Marek Kohn <marek.kohn@gmx.net>
2018-03-19 21:52:39 +0100

    Add commandline argument to specify the module

--- get_splan.py
+++ get_splan.py
@@ -1,19 +1,32 @@
-times = {1 : '08:15 - 09:45', 2 : '10:00 - 11:30', 3 : '11:45 - 13:15', 4 : '14:15 - 15:45', 5 : '16:00 - 17:30', 6 : '17:45 - 19:15'}
+#!/usr/bin/env python3
+
+import urllib.request
+import sys, getopt
+
+url='https://raumservice.htwsaar.de/userfunctions/splan/splan_person_download.php'
+
+times = {
+	1 : '08:15 - 09:45', 
+	2 : '10:00 - 11:30', 
+	3 : '11:45 - 13:15', 
+	4 : '14:15 - 15:45', 
+	5 : '16:00 - 17:30', 
+	6 : '17:45 - 19:15'
+}
 
 class Module:
-
 	def __init__(self, row):
 		self.day = row[0]
 		self.hour = int(row[1])
-		self.name = row[2]
+		self.name = row[2].replace('"', '')
 		self.nr = row[3]
 		self.faculty = row[4]
 		self.room = row[5]
-		self.comment = row[6]
+		self.comment = row[6].replace('"', '')
 		self.time = times[self.hour]
 
 	def __repr__(self):
-		return self.day + ' ' + str(self.hour) + ' ' + self.time + ' ' + self.name + ' ' + self.nr + ' ' + self.faculty + ' ' + self.room + ' ' + self.comment
+		return ' '.join(self.day, str(self.hour), self.time, self.name, self.nr, self.faculty, self.room, self.comment)
 
 	def __eq__(self, other):
 		return (self.nr == other.nr) and (self.hour == other.hour)
@@ -27,67 +40,59 @@ class Module:
 	def prettyprint(this_object):
 		this_object.__prettyprint__()
 
-url='https://raumservice.htwsaar.de/userfunctions/splan/splan_person_download.php'
-file_name='stundenplan.csv'
+def get_schedule(for_module):
+	data = urllib.request.urlopen(url)
+	csv = str(data.read().decode('latin1'))
+	lines = csv.split("\n")
+	lines.pop(0)
 
-import urllib.request
-with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
-    data = response.read() # a `bytes` object
-    out_file.write(data)
+	modules = {'Mo':[], 'Di':[], 'Mi':[], 'Do':[], 'Fr':[]}
 
-modules = []
-mon = []
-tue = []
-wed = []
-thu = []
-fri = []
+	for l in lines:
+		row = l.split(";")
+		if len(row) >=4 and row[4] == for_module and row[2] != '':
+			module = Module(row)
+			if(module.day in modules):
+				modules[module.day].append(module)
 
-import csv
-with open ('stundenplan.csv', encoding='latin1') as splan:
-	reader = csv.reader(splan, delimiter=';')
-	for row in reader:
-		if row[4] == 'PIM' and row[2] != '':
-			modules.append(Module(row))
+	print('Montag____________________________________________________')
+	for module in sorted(modules['Mo'], key=lambda module: module.hour):
+		module.prettyprint()
+	print('')
 
-for module in list(set(modules)):
-	if(module.day == 'Mo'):
-		mon.append(module)
-	elif(module.day == 'Di'):
-		tue.append(module)
-	elif(module.day == 'Mi'):
-		wed.append(module)
-	elif(module.day == 'Do'):
-		thu.append(module)
-	elif(module.day == 'Fr'):
-		fri.append(module)
+	print('Dienstag__________________________________________________')
+	for module in sorted(modules['Di'], key=lambda module: module.hour):
+		module.prettyprint()
+	print('')
 
-print('Montag____________________________________________________')
-for module in sorted(mon, key=lambda module: module.hour):
-	module.prettyprint()
-print('')
+	print('Mittwoch__________________________________________________')
+	for module in sorted(modules['Mi'], key=lambda module: module.hour):
+		module.prettyprint()
+	print('')
 
-print('Dienstag__________________________________________________')
-for module in sorted(tue, key=lambda module: module.hour):
-	module.prettyprint()
-print('')
+	print('Donnerstag________________________________________________')
+	for module in sorted(modules['Do'], key=lambda module: module.hour):
+		module.prettyprint()
+	print('')
 
-print('Mittwoch__________________________________________________')
-for module in sorted(wed, key=lambda module: module.hour):
-	module.prettyprint()
-print('')
-
-print('Donnerstag________________________________________________')
-for module in sorted(thu, key=lambda module: module.hour):
-	module.prettyprint()
-print('')
-
-print('Freitag___________________________________________________')
-for module in sorted(fri, key=lambda module: module.hour):
-	module.prettyprint()
-print('')
+	print('Freitag___________________________________________________')
+	for module in sorted(modules['Fr'], key=lambda module: module.hour):
+		module.prettyprint()
+	print('')	
 
 
+def main(argv): 
+	module = ''
+	try:
+		opts, args = getopt.getopt(argv,"m:")
+	except getopt.GetoptError:
+		print('get_schedule.py -m <MODULE>')
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-m':
+			module = arg 
+			get_schedule(module)
 
 
-
-
+if __name__ == "__main__":
+   main(sys.argv[1:])
